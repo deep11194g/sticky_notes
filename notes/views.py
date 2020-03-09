79341937ecbca7c3da6a158c0f0ebf4c1a35db3e
deep11194g <soumyadeep11194@gmail.com>
2020-03-09 13:21:50 +0100

    made form action for new note creation

--- notes/views.py
+++ notes/views.py
@@ -6,7 +6,14 @@ from .models import Note
 
 def index(request):
     """Controller function for home page"""
-    notes = Note.objects.order_by('-created_at')
+    if request.method == "POST":
+        new_note = request.POST.get('new_text')
+        if not new_note:
+            print(request.POST)
+            return HttpResponse('New Note is empty', status=400)
+        note = Note(text=new_note)
+        note.save()
+    notes = Note.objects.all().order_by('-created_at')
     template = loader.get_template('index.html')
     context = {'notes': notes}
     return HttpResponse(template.render(context, request))
